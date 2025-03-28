c95fdf9d8bbc4a06032b91d5f15f83a2b0349ab7
Andre A Mendes <andreazevedomendes@gmail.com>
2025-03-28 07:43:39 -0300

    Toda a pagina foi finalizada, CSS , JS e envio de formulario

---
+++ function.js
@@ -0,0 +1,27 @@
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('input[name="nome"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+
+  fetch('https://api.sheetmonkey.io/form/sanff8bXPVahtsrYEKE4Kr', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ Name: name, Email: email })
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log('Success:', data);
+    alert("Formulário enviado com sucesso!");
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    alert("Erro ao enviar o formulário.");
+  });
+};
+
+
+document.querySelector("#formulario").addEventListener('submit', handleSubmit);
